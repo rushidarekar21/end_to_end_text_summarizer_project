7d2845dc7ec20045f8789b7673af0bc889c486b4
Rushi <rushidarekar21@gmail.com>
2025-02-03 20:12:00 +0530

    Project Completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion import DataIngestionPipeline
 from src.textSummarizer.pipeline.stage_2_data_tranformation import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.stage_3_model_trainer import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.stage_4_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -36,3 +37,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
